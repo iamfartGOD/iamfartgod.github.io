9be0b5212ab05e9e54c8d6740838d996c1c8ae96
iamfartGOD <iamfartgod@gmail.com>
2025-02-13 17:42:36 -0800

    Update script.js

--- script.js
+++ script.js
@@ -5,8 +5,8 @@ document.getElementById('reverb-slider').addEventListener('input', (event) => {
     reverb.wet.value = event.target.value;
 });
 
-async function playFart() {
-    const player = new Tone.Player("https://www.dropbox.com/scl/fi/t6hcklj6s6kmei5ps3qmc/Fart27.wav?rlkey=595brlojrpkqv52im7injknad&st=8vca83fc&dl=1").connect(reverb);
-    await Tone.start();
-    player.start();
+function playFart() {
+  const player = new Tone.Player("https://www.dropbox.com/scl/fi/t6hcklj6s6kmei5ps3qmc/Fart27.wav?rlkey=595brlojrpkqv52im7injknad&st=nq4jhfyt&dl=1").toDestination();
+  player.start();
 }
+
